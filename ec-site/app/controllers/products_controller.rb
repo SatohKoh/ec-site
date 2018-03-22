bf482d4d80f5a532a847e397a2b6056c3b11934c
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2018-03-22 16:10:45 +0900

    add: dbから画像ファイルをアップデートできるようにした

--- ec-site/app/controllers/products_controller.rb
+++ ec-site/app/controllers/products_controller.rb
@@ -79,6 +79,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :price, :weight, :stock, :detail)
+      params.require(:product).permit(:name, :price, :weight, :stock, :detail, :image)
     end
 end
