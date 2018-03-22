f831457eca5cdfbcfc7b78ba11a2f20c59e38633
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2018-03-22 15:36:26 +0900

    add: 購入するお米の詳細カラム

--- ec-site/app/views/products/_product.json.jbuilder
+++ ec-site/app/views/products/_product.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! product, :id, :name, :price, :weight, :stock, :created_at, :updated_at
+json.extract! product, :id, :name, :price, :weight, :stock, :detail, :created_at, :updated_at
 json.url product_url(product, format: :json)
--- ec-site/db/schema.rb
+++ ec-site/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322045017) do
+ActiveRecord::Schema.define(version: 20180322061742) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180322045017) do
     t.integer "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "detail"
   end
 
 end
